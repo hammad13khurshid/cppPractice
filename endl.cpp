322044482c67f4e0cae998e0777da10d8a9df1de
hammad13khurshid <hammadkhurshid931@gmail.com>
2023-09-11 02:20:04 +0500

    learned and added code for Endl function

---
+++ endl.cpp
@@ -0,0 +1,11 @@
+
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cout<<"Hi, My name is Hammad Khurhid" << endl;  //endl adds a new line for the output.
+    cout << "I've just added a new line using endl";
+}
