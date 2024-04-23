e9e1c733ff5a5e9b9c3aab30eb6f7ce8c05a90f1
hammad13khurshid <hammadkhurshid931@gmail.com>
2024-04-23 22:53:02 +0500

    Learned new topic 'Variables' in C++ and their implementation

---
+++ variables.cpp
@@ -0,0 +1,29 @@
+//This program demonstrates the concept of variables in C++.
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Hammad Khurshid";
+    //string is used to store combination of characters in one variable
+
+    int rollNo = 102;
+    //int is used to store integer values like postive and negative numbers.
+
+    char group = 'A';
+    //char is used to store a single character.
+
+    bool isMale = true;
+    //bool is used to store true or false values.
+
+    // Set boolalpha to display true/false instead of 1/0
+    cout << boolalpha;
+    
+    cout <<"Student Name: "<< name << endl;
+    cout <<"Roll No: "<<rollNo << endl;
+    cout <<"Group:  "<< group << endl;
+    cout <<"Gender is Male: "<<isMale << endl;
+    //Values stored in the variables can be printed or desplayed on output screen by calling the name of the variables. 
+}
